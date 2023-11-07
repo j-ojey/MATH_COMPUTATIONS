65a92101d62975c1576b0de52d0079f293bbdda9
Festus Gitahi <festusgitahik@gmail.com>
2023-11-07 11:52:32 +0300

    feat: added mulltiplication and division classes

--- math/Arithmetics.java
+++ math/Arithmetics.java
@@ -1,16 +1,38 @@
 package math;
 
-class Addition{
+class Addition {
 	int i;
 	int j;
 	int add;
-	
-	public int Add(int a, int b){
-		add=a+b;
+
+	public int Add(int a, int b) {
+		add = a + b;
 		return add;
-		
+
 	}
-	
+
+}
+
+class Mulitiplcation {
+	int i;
+	int j;
+	int mul;
+
+	public int Mul(int a, int b) {
+		mul = a * b;
+		return mul;
+
+	}
+
+}
+class Division{
+	int i;
+	int j;
+	int div;
+	public int Div(int a, int b) {
+		div = a / b;
+		return div;
+	}
 }
 
 public class Arithmetics {
@@ -19,7 +41,6 @@ public class Arithmetics {
 		System.out.println("Hello World");
 		Addition sum = new Addition();
 		System.out.println(sum.Add(1, 0));
-		
 
 	}
 
