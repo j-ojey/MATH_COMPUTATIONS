887a429624d02e50bf6caeb4ed14df9bebe52cd9
mesh-dell <mwaimonicas@gmail.com>
2023-11-07 22:53:43 +0300

    Fixed bugs and implemented circle perimeter class

--- math/Arithmetics.java
+++ math/Arithmetics.java
@@ -41,133 +41,162 @@ class Division {
 }
 class Subtraction {
 
-		int k;
-		int l;
-		int sub;
+	int k;
+	int l;
+	int sub;
 
-		public int Sub(int c, int d) {
-			sub = c - d;
-			return sub;
-		}
+	public int Sub(int c, int d) {
+		sub = c - d;
+		return sub;
 	}
+}
 
-	class Rectangle {
-		int l;
-		int w;
-		int per;
+class Rectangle {
+	int l;
+	int w;
+	int per;
 
-		public int per(int l, int w) {
-			per = 2 * (l + w);
-			return per;
-		}
+	public int per(int l, int w) {
+		per = 2 * (l + w);
+		return per;
 	}
+}
+
+class Circle{
+	double r;
+	double pi=3.142;
+	double area;
+	 public double area(double r) {
+		 double d = pi*r;
+		 area= d*r;
+		 return area;
+	 }
+}
 	
-	class Circle{
-		double r;
-		double pi=3.142;
-		double area;
-		 public double are(double r) {
-			 double d = pi*r;
-			 area= d*r;
-			 return area;
-		 }
+class Triangle{
+	int y;
+	int h;
+	int area;
+
+	public int tri(int y, int h){
+		area = (int) (0.5 * y * h);
+		return area;
+
 	}
+}
 	
-	class Triangle{
-		int y;
-		int h;
-		int area;
-		
-		public int tri(int y, int h){
-			area=(int) 0.5*y*h;
-			return area;
-			
-		}
+class CirclePerimeter
+{
+	double circ, pi = 3.142;
+	public double circumfrence(double radius)
+	{
+		circ = pi * (radius * 2);
+		return circ;
 	}
-	
+}
 public class Arithmetics {
 
 	public static void main(String[] args) {
+		boolean isRunning = true;
 		System.out.println("Hello World");
-		
-		while(true) {
-		System.out.println(("1. Addition"));
-		System.out.println(("2. Subtraction"));
-		System.out.println(("3. Multiplication"));
-		System.out.println(("4. Division"));
-		System.out.println("5. Rectangle Perimeter");
-		System.out.println("6. Triangle Area");
-		System.out.println("7. Circle Area");
-		System.out.println("8. Circle Perimeter");
-		System.out.println("9. Exit");
-		System.out.println("Welcome to Arithmetics, Choose an option:");
 		Scanner sc = new Scanner(System.in);
-		int option = sc.nextInt();
-		
-		}
 
-		switch (option) {
-			case 1:
-				System.out.println("Enter first number:");
-				int a = sc.nextInt();
-				System.out.println("Enter second number:");
-				int b = sc.nextInt();
-				Addition add = new Addition();
-				int results = add.Add(a, b);
-				System.out.println("The sum is" + " " + results);
-				break;
-				
-			case 2:
+		while (isRunning) {
+			System.out.println(("1. Addition"));
+			System.out.println(("2. Subtraction"));
+			System.out.println(("3. Multiplication"));
+			System.out.println(("4. Division"));
+			System.out.println("5. Rectangle Perimeter");
+			System.out.println("6. Triangle Area");
+			System.out.println("7. Circle Area");
+			System.out.println("8. Circle Perimeter");
+			System.out.println("9. Exit");
+			System.out.println("Welcome to Arithmetics, Choose an option:");
 
-				System.out.println("Enter first number");
-				int c=sc.nextInt();
-				System.out.println("Enter the second number");
-				int d=sc.nextInt();
-				Subtraction sub = new Subtraction();
-				int result=sub.Sub(c, d);
-				System.out.println("Your answer is"+result);
-				break;
-				
-			case 3:
-				System.out.println("Enter the first number");
-				int  e=sc.nextInt();
-				System.out.println("Enter the second number");
-				int f=sc.nextInt();
-				Division div=new Division();
-				int answer=div.Div(e, f);
-				System.out.println("Result:"+answer);
-				break;
+			int option = sc.nextInt();
 
-			case 5:
-				System.out.println("Enter the length of the rectangle");
-				int l = sc.nextInt();
-				System.out.println("Enter the width of the rectangle");
-				int w = sc.nextInt();
-				Rectangle rec = new Rectangle();
-				int perimeter = rec.per(l, w);
-				System.out.println("The perimeter of the rectangle is" + " " + perimeter);
-				break;
-	
-			case 6:
-				System.out.println("Enter the base:");
-				int y = sc.nextInt();
-				System.out.println("Enter the height:");
-				int h =sc.nextInt();
-				Triangle tr = new Triangle();
-				System.out.println("The area of the triangle is" + " " + tr.tri(y,h));
-				break;
-				
-			case 7:
-				System.out.println("Enter the radius:");
-				int r= sc.nextInt();
-				Circle cir = new Circle();
-				System.out.println("The area of the circle is" + " " + cir.are(r));
-				break;
-				
-				
-				
-			default:
-				break;
+			switch (option) {
+				case 1:
+					System.out.println("Enter first number:");
+					int a = sc.nextInt();
+					System.out.println("Enter second number:");
+					int b = sc.nextInt();
+					Addition add = new Addition();
+					int results = add.Add(a, b);
+					System.out.println("The sum is" + " " + results);
+					break;
+
+				case 2:
+
+					System.out.println("Enter first number");
+					int c = sc.nextInt();
+					System.out.println("Enter the second number");
+					int d = sc.nextInt();
+					Subtraction sub = new Subtraction();
+					int result = sub.Sub(c, d);
+					System.out.println("Your answer is" + result);
+					break;
+
+
+				case 3:
+					System.out.println("Enter the first number");
+					int x = sc.nextInt();
+					System.out.println("Enter the second number");
+					int j = sc.nextInt();
+					Mulitiplcation mult = new Mulitiplcation();
+					int ans = mult.Mul(x, j);
+					System.out.println("Result:" + ans);
+					break;
+
+				case 4:
+					System.out.println("Enter the first number");
+					int e = sc.nextInt();
+					System.out.println("Enter the second number");
+					int f = sc.nextInt();
+					Division div = new Division();
+					int answer = div.Div(e, f);
+					System.out.println("Result:" + answer);
+					break;
+
+				case 5:
+					System.out.println("Enter the length of the rectangle");
+					int l = sc.nextInt();
+					System.out.println("Enter the width of the rectangle");
+					int w = sc.nextInt();
+					Rectangle rec = new Rectangle();
+					int perimeter = rec.per(l, w);
+					System.out.println("The perimeter of the rectangle is" + " " + perimeter);
+					break;
+
+				case 6:
+					System.out.println("Enter the base:");
+					int y = sc.nextInt();
+					System.out.println("Enter the height:");
+					int h = sc.nextInt();
+					Triangle tr = new Triangle();
+					System.out.println("The area of the triangle is" + " " + tr.tri(y, h));
+					break;
+
+				case 7:
+					System.out.println("Enter the radius:");
+					int r = sc.nextInt();
+					Circle cir = new Circle();
+					System.out.println("The area of the circle is" + " " + cir.area(r));
+					break;
+
+				case 8:
+					System.out.println("Enter the radius:");
+					double radius = sc.nextDouble();
+					CirclePerimeter circlePerimeter = new CirclePerimeter();
+					System.out.println("The circumfrence of the circle is" + " " + circlePerimeter.circumfrence(radius) + "\n");
+					break;
+
+				case 9:
+					isRunning = false;
+					break;
+				default:
+					System.out.println("Enter a valid option\n");
+			}
 		}
 
 		sc.close();
