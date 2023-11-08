111c5eb14fc9ef8eafe34e87a68e98f01b223d2c
stilldrey2 <emmanuelkipkoech742@gmail.com>
2023-11-08 10:35:30 +0300

    CircleArea commit

---
+++ math/CircleArea.java
@@ -0,0 +1,12 @@
+package math;
+
+public class CircleArea {
+	double r;
+	double pi=3.142;
+	double area;
+	 public double area(double r) {
+		 double d = pi*r;
+		 area= d*r;
+		 return area;
+	 }
+}
